05ff4bb128cd65ff8af24c8817151bd27e562e81
Ayush Mehrotra <ayushmehrotra1234@gmail.com>
2020-11-03 23:56:22 +0530

    While loop

---
+++ While.py
@@ -0,0 +1,21 @@
+count = 0
+
+while (count < 3):      
+
+    count = count + 1
+
+    print("Hello Geek")  
+
+  
+
+print() 
+
+  
+# checks if list still 
+# contains any element  
+
+a = [1, 2, 3, 4] 
+
+while a: 
+
+    print(a.pop()) 
